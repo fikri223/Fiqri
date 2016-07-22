8dde0d36db8d3ad72c727263687750a660269caa
fikri223 <muh_fikri@outlook.com>
2016-07-22 22:10:41 +0800

    Add files via upload

---
+++ www/js/index_user_scripts.js
@@ -0,0 +1,55 @@
+/*jshint browser:true */
+/*global $ */(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  .uib_w_8 */
+    
+    
+        /* button  Button */
+    
+    
+        /* button  Button */
+    
+    
+        /* button  Keluar */
+    $(document).on("click", ".uib_w_13", function(evt)
+    {
+      
+         return false;
+    });
+    
+        /* button  Keluar */
+    $(document).on("click", ".uib_w_13", function(evt)
+    {
+       
+         return false;
+    });
+    
+        /* button  Button */
+    $(document).on("click", ".uib_w_11", function(evt)
+    {
+      
+        };
+        $.afui.popup(options); 
+         return false;
+    });
+    
+        /* button  Button */
+    $(document).on("click", ".uib_w_11", function(evt)
+    {
+  
+        };
+        $.afui.popup(options); 
+         return false;
+    });
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
